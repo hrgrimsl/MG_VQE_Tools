4e70698c652299ece2746420fa3fe4789893afe7
Harper <hrgrimsl@vt.edu>
2019-01-08 20:59:44 -0500

    Fixed two inputs and added LADAPT and RADAPT

--- methods.py
+++ methods.py
@@ -1,18 +1,25 @@
 from evaluations import *
 import copy
 from Classes import *
+import random
 
 
 def Optimize(molecule, ops, logging, **kwargs):
     algorithm = kwargs.get('algorithm', 'VQE')
     theta_tightness = float(kwargs.get('theta_tightness', '1e-5'))
     ADAPT_tightness = float(kwargs.get('ADAPT_tightness', '1e-5'))
+    seed = float(kwargs.get('RADAPT_seed', 0))
     if algorithm == 'VQE':
         outcome = VQE(molecule, ops, theta_tightness)
     if algorithm == 'ADAPT':
         outcome = ADAPT(molecule, ops, theta_tightness, ADAPT_tightness, logging)
     if algorithm == 'UCC':
         outcome = UCC(molecule, ops, theta_tightness)
+    if algorithm == 'RADAPT':
+        outcome = RADAPT(molecule, ops, theta_tightness, ADAPT_tightness, logging, seed)
+    if algorithm == 'LADAPT':
+        outcome = LADAPT(molecule, ops, theta_tightness, ADAPT_tightness, logging)
+
     return outcome
 
 def VQE(molecule, ops, theta_tightness):
@@ -23,6 +30,7 @@ def VQE(molecule, ops, theta_tightness):
     print('Performing optimization of parameters...')
     optimization = scipy.optimize.minimize(Trotter_SPE, parameters, jac = Trotter_Gradient, args = (ops), method = 'BFGS', options = {'gtol': float(theta_tightness), 'disp': False})
     print(str(len(parameters))+' parameters optimized in '+str(optimization.nit)+' iterations!')
+    print('Current energy: '+str(optimization.fun))
     return optimization
 
 def ADAPT(molecule, ops, theta_tightness, ADAPT_tightness, logging):
@@ -54,6 +62,68 @@ def ADAPT(molecule, ops, theta_tightness, ADAPT_tightness, logging):
         current_ket = copy.copy(ops.HF_ket)
         for i in reversed(range(0, len(parameters))):
              current_ket = scipy.sparse.linalg.expm_multiply(parameters[i]*ansatz.Full_JW_Ops[i], current_ket) 
+    return OptRes
+
+def RADAPT(molecule, ops, theta_tightness, ADAPT_tightness, logging, seed):
+    random.seed()
+    ansatz = Ansatz_Operations(ops)
+    current_ket = copy.copy(ops.HF_ket)
+    parameters = []
+    gradients = [None]
+    while gradients[-1] == None or abs(gradients[-1])>ADAPT_tightness:
+        grad = 0
+        num = None
+        hbra = current_ket.transpose().conj().dot(ops.JW_hamiltonian)
+        vector = []
+        num = random.randint(0, len(ops.Full_JW_Ops))
+        for i in range(0, len(ops.Full_JW_Ops)):
+            comm = 2*hbra.dot(ops.Full_JW_Ops[i]).dot(current_ket).toarray()[0][0].real
+            vector.append(comm) 
+            if i == num:
+                grad = comm
+        print('Next operation: '+str(ops.Full_Ops[num]))
+        print('Next gradient: '+str(grad))
+        gradients.append(scipy.linalg.norm(vector))
+        print('Norm of all gradients: '+str(gradients[-1]))
+        ansatz.Full_JW_Ops.insert(0, ops.Full_JW_Ops[num])
+        ansatz.Full_Ops.insert(0, ops.Full_Ops[num])
+        OptRes = VQE(molecule, ansatz, theta_tightness)
+        parameters = OptRes.x
+        energy = OptRes.fun
+        current_ket = copy.copy(ops.HF_ket)
+        for i in reversed(range(0, len(parameters))):
+             current_ket = scipy.sparse.linalg.expm_multiply(parameters[i]*ansatz.Full_JW_Ops[i], current_ket) 
+    return OptRes
+
+def LADAPT(molecule, ops, theta_tightness, ADAPT_tightness, logging):
+    random.seed()
+    ansatz = Ansatz_Operations(ops)
+    current_ket = copy.copy(ops.HF_ket)
+    parameters = []
+    gradients = [None]
+    num = 0
+    while (gradients[-1] == None or abs(gradients[-1])>ADAPT_tightness) and num<len(ops.Full_JW_Ops):
+        grad = 0
+        hbra = current_ket.transpose().conj().dot(ops.JW_hamiltonian)
+        vector = []
+        for i in range(0, len(ops.Full_JW_Ops)):
+            comm = 2*hbra.dot(ops.Full_JW_Ops[i]).dot(current_ket).toarray()[0][0].real
+            vector.append(comm) 
+            if i == num:
+                grad = comm
+        print('Next operation: '+str(ops.Full_Ops[num]))
+        print('Next gradient: '+str(grad))
+        gradients.append(scipy.linalg.norm(vector))
+        print('Norm of all gradients: '+str(gradients[-1]))
+        ansatz.Full_JW_Ops.insert(0, ops.Full_JW_Ops[num])
+        ansatz.Full_Ops.insert(0, ops.Full_Ops[num])
+        OptRes = VQE(molecule, ansatz, theta_tightness)
+        parameters = OptRes.x
+        energy = OptRes.fun
+        current_ket = copy.copy(ops.HF_ket)
+        for i in reversed(range(0, len(parameters))):
+             current_ket = scipy.sparse.linalg.expm_multiply(parameters[i]*ansatz.Full_JW_Ops[i], current_ket) 
+        num+=1
     return OptRes
 
 def UCC(molecule, ops, theta_tightness):
