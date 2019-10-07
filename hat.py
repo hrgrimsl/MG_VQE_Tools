97c7981619ec62c2e0b164db676281a186560b2f
hrgrimsl <40306884+hrgrimsl@users.noreply.github.com>
2019-10-07 15:57:19 -0400

    Update hat.py
    hat

--- hat.py
+++ hat.py
@@ -3,7 +3,7 @@ def hat():
     print('  **    ')
     print('  * *  -\"I cast magic missile at the darkness!\"')
     print(' *****              -1d4')
-    print('  * *  @                     -1d4 ')
+    print(' *   *  @                     -1d4 ')
     print('   *   |      -1d4                         The Darkness        ') 
     print('  *****|      ')
     print(' * *   |                 -1d4 ')
